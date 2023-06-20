1f9881dbab726d0305d7bc722a008c36e348b708
Aadesh <aadesh.connect@gmail.com>
2023-06-20 12:16:21 +0530

    added Authentication

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,42 +7,59 @@ import {
   NavBtn,
   NavBtnLink,
 } from "./NavbarElements";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Navbar = ({ loggedIn }) => {
+const Navbar = () => {
+  const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
-  
   return (
     <>
-        <Nav>
-          <NavLink to="/">
-            {/* <img
+      <Nav>
+        <NavLink to="/">
+          {/* <img
               src={require("https://raw.githubusercontent.com/briancodex/react-navbar-v3/642eefd7f46c977087898c4ba0a3dc5564dd301c/src/images/logo.svg")}
               alt="logo"
             /> */}
-            <h1>Home</h1>
+          <h1>Home</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
           </NavLink>
-          <Bars />
-          <NavMenu>
-            <NavLink to="/about" activeStyle>
-              About
-            </NavLink>
-            <NavLink to="/policy" activeStyle>
-              Policy
-            </NavLink>
-            <NavLink to="/contact" activeStyle>
-              Contact Us
-            </NavLink>
-            <NavLink to="/faqs" activeStyle>
-              FAQs
-            </NavLink>
-            <NavLink to="/signup" activeStyle>
-              Sign Up
-            </NavLink>
-          </NavMenu>
+          <NavLink to="/policy" activeStyle>
+            Policy
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/faqs" activeStyle>
+            FAQs
+          </NavLink>
+          { isAuthenticated && (
+          <NavLink to="/myprofile" activeStyle>
+            My Profile
+          </NavLink>)
+          }
+        </NavMenu>
+        {isAuthenticated ? (
           <NavBtn>
-            <NavBtnLink to="/login">Log In</NavBtnLink>
+            <NavBtnLink
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Log Out
+            </NavBtnLink>
           </NavBtn>
-        </Nav>
+        ) : (
+          <NavBtn>
+            <NavBtnLink onClick={() => loginWithRedirect()}>
+              Log In/Register
+            </NavBtnLink>
+          </NavBtn>
+        )}
+      </Nav>
     </>
   );
 };
