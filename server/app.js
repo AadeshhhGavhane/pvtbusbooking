e9b28da5bf4b4ed81089a1de9a7f66a7e696ef53
Apurv-15 <95355502+Apurv-15@users.noreply.github.com>
2023-07-17 09:48:27 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,35 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const router = require("./routes/router")
-const cors = require("cors")
+const router = require("./routes/router");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const port = 8006;
 
+// Create a database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+   password: "root",
+  database: "test",
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
+
 app.use(express.json());
 app.use(cors());
-app.use(router)
+app.use(router);
 
-app.listen(port,()=>{
-    console.log(`server start at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
+
+
